1ac6752243bfc8547cc631dde3f5b99399163d86
Ivana <ivana.gyro@gmail.com>
2023-04-04 20:00:31 +0800

    Acutally import athlete results to BigQuery
    The functions for transforming types to string are moved to
    `src/locales` to avoid duplicated definition in multiple exporters.

---
+++ src/locales/zh.ts
@@ -0,0 +1,71 @@
+import {
+  Game, Sport, EventType, Category, Round,
+} from '../SportEvent';
+
+import { Gender, Country } from '../AthleteResult';
+
+function gameToString(game: Game): string {
+  const mapping = {
+    [Game.TAIWAN_INTERCOLLEGIATE_GAMES]: '全大運',
+  };
+  return mapping[game];
+}
+
+function sportToString(sport: Sport): string {
+  const mapping = {
+    [Sport.FIELD_AND_TRACK]: '田徑',
+  };
+  return mapping[sport];
+}
+
+function eventTypeToString(eventType: EventType): string {
+  const mapping = {
+    [EventType.RUN_400_METRES]: '400 公尺',
+  };
+  return mapping[eventType];
+}
+
+function categoryToString(category: Category): string {
+  const mapping = {
+    [Category.FEMALE]: '女子組',
+    [Category.MALE]: '男子組',
+    [Category.MIXED]: '混合',
+    [Category.OPEN]: '公開組',
+  };
+  return mapping[category];
+}
+
+function roundToString(round?: Round): string | undefined {
+  const mapping = {
+    [Round.PRELIMINARY]: '預賽',
+    [Round.SEMI_FINAL]: '準決賽',
+    [Round.FINAL]: '決賽',
+  };
+  return round && mapping[round];
+}
+
+function genderToString(gender: Gender): string {
+  const mapping = {
+    [Gender.MALE]: '男',
+    [Gender.FEMALE]: '女',
+    [Gender.NON_BINARY]: '非二元',
+  };
+  return mapping[gender];
+}
+
+function countryToString(country: Country): string {
+  const mapping = {
+    [Country.TAIWAN]: '台灣',
+  };
+  return mapping[country];
+}
+
+export {
+  gameToString,
+  sportToString,
+  eventTypeToString,
+  categoryToString,
+  roundToString,
+  genderToString,
+  countryToString,
+};
